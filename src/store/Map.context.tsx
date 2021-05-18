85913afe42da9cba592c68e802e8f1bbc9482940
LaFusew <antoine.oddoz@gmail.com>
2021-05-18 09:16:02 +0200

    -client map stored as context to allow easymodification/testing

--- src/store/Map.context.tsx
+++ src/store/Map.context.tsx
@@ -3,4 +3,5 @@ import React from 'react';
 export type Row = string[];
 export type Map = Row[];
 
+//this context store the map and allow from map editing to testing.
 export const FullMapInfo = React.createContext<Map>([]);
