90d15b2cd3a0ab014f42ea035490938c60a67d56
LaFusew <antoine.oddoz@gmail.com>
2021-05-19 10:44:36 +0200

    -Tile interface precised

--- src/store/Map.context.tsx
+++ src/store/Map.context.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
-export type Row = string[];
+export interface Tile {
+  isCollider: boolean;
+  type: string;
+  rotation: number;
+}
+
+export type TileKeys = 'isCollider' | 'type' | 'rotation'
+
+export type Row = Tile[];
 export type Map = Row[];
 
 //this context store the map and allow from map editing to testing.
